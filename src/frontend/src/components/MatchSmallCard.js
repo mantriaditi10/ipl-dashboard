566266a0fbad7cfc18260a678de0edb4ea19114e
Aditi Mantri <mantriaditi10@gmail.com>
2021-07-20 09:39:16 +0530

    Team Page remaining

--- src/frontend/src/components/MatchSmallCard.js
+++ src/frontend/src/components/MatchSmallCard.js
@@ -1,9 +1,10 @@
 import React from 'react'
 
-export const MatchSmallCard = ({match}) => {
+export const MatchSmallCard = ({teamName, match}) => {
+    const otherTeam = match.team1 === teamName ? match.team2 : match.team1;
     return (
         <div className="MatchSmallCard">
-            <p>{match.team1} vs {match.team2}</p>
+            <p>vs {match.team2}</p>
         </div>
     )
 }
