c994635e81ea54a063b153936a3c04d03b6e6b38
Aditi Mantri <mantriaditi10@gmail.com>
2021-07-21 20:20:25 +0530

    Added Backend Match API

--- src/main/java/io/aditi/ipldashboard/controller/TeamController.java
+++ src/main/java/io/aditi/ipldashboard/controller/TeamController.java
@@ -1,11 +1,16 @@
 package io.aditi.ipldashboard.controller;
 
+import java.time.LocalDate;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.aditi.ipldashboard.model.Match;
 import io.aditi.ipldashboard.model.Team;
 import io.aditi.ipldashboard.repository.MatchRepository;
 import io.aditi.ipldashboard.repository.TeamRepository;
@@ -25,5 +30,17 @@ public class TeamController {
         Team team =  teamRepository.findByTeamName(teamName);
         team.setMatches(matchRepository.findLatestMatchesByTeam(teamName, 4));
         return team;
-    }    
+    }  
+    
+    @GetMapping("/team/{teamName}/matches")
+    public List<Match> getMatchesForTeam(@PathVariable String teamName, @RequestParam int year){
+        LocalDate startDate = LocalDate.of(year, 1, 1);
+        LocalDate endDate = LocalDate.of(year + 1, 1, 1);
+        // return matchRepository.getByTeam1AndDateBetweenOrTeam2AndDateBetweenOrderByDateDesc(
+        //     teamName, startDate, endDate,
+        //     teamName, startDate, endDate            
+        // );
+        return matchRepository.getMatchesByTeamBetweenDates(teamName, startDate, endDate);
+    }
+
 }
